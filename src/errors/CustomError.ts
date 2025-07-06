3d58a0fe69a52f5c5c06a068dffd65daf8468033
reinaldo bustamante <reinaldo.bustamante026@gmail.com>
2025-07-06 13:15:19 -0400

    feat: add unauthorized, notfound errors

--- src/errors/CustomError.ts
+++ src/errors/CustomError.ts
@@ -10,6 +10,14 @@ export class CustomError extends Error {
     return new CustomError(message, 400);
   }
 
+  public static unauthorized(message: string) {
+    return new CustomError(message, 401);
+  }
+
+  public static notFound(message: string) {
+    return new CustomError(message, 404);
+  }
+
   public static conflict(message: string) {
     return new CustomError(message, 409);
   }
