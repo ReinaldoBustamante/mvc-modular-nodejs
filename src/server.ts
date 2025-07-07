923eb28e115808fcdc4139a2a2b5682498f9c61f
reinaldo bustamante <reinaldo.bustamante026@gmail.com>
2025-07-07 10:43:47 -0400

    feat: add swagger api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import express, { Router } from "express";
+import { ErrorMiddleware } from "./middlewares/error.middleware";
+import swaggerUI from 'swagger-ui-express';
+import fs from 'fs'
+import YAML from 'yaml'
 
 export class AppServer {
   constructor(
@@ -8,8 +12,14 @@ export class AppServer {
 
   public start() {
     const app = express();
+    const file = fs.readFileSync('./docs/swagger.yaml', 'utf-8');
+    const swaggerDocument = YAML.parse(file);
+
     app.use(express.json());
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
     app.use("/api", this.routes);
+    app.use(ErrorMiddleware.errorHandler);
+
     app.listen(this.port, () =>
       console.log(`Server running on port ${this.port}`),
     );
