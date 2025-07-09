bb2ae906fa8907bc7bc90f5890361b58a9462644
reinaldo bustamante <reinaldo.bustamante026@gmail.com>
2025-07-09 16:38:25 -0400

    feat: role propety change to optional

--- src/modules/auth/dtos/registerUser.dto.ts
+++ src/modules/auth/dtos/registerUser.dto.ts
@@ -2,14 +2,14 @@ export interface RegisterUserProps {
   name: string;
   email: string;
   password: string;
-  role: string;
+  role?: string;
 }
 
 export class RegisterUserDto {
   public name: string;
   public email: string;
   public password: string;
-  public role: string;
+  public role?: string;
 
   constructor(data: RegisterUserProps) {
     this.name = data.name;
